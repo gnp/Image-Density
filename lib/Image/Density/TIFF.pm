5d07b936d0ad4309f937ebfa3757c2c5b6e72889
Gregor N. Purdy, Sr <gnp@acm.org>
2012-04-30 00:50:33 -0700

    Port to MAS::TIFF from Inline::C and libtiff

--- lib/Image/Density/TIFF.pm
+++ lib/Image/Density/TIFF.pm
@@ -69,14 +69,9 @@ use warnings 'all';
 
 package Image::Density::TIFF;
 
-our $VERSION = '0.3';
+use MAS::TIFF::File;
 
-use Inline (
-  C            => 'DATA',
-  LIBS         => '-ltiff',
-  AUTO_INCLUDE => '#include <stdio.h>',
-  AUTO_INCLUDE => '#include <tiffio.h>'
-);
+our $VERSION = '0.3';
 
 BEGIN {
   use Exporter;
@@ -85,288 +80,148 @@ BEGIN {
   @EXPORT = qw(&tiff_density &tiff_densities);
 }
 
-1;
+my $MARGIN_FACTOR = 20;
 
-__DATA__
-__C__
+sub tiff_directory_density {
+  use integer;
+  
+  my $t = shift;
+  
+  die "Could not open file for reading" unless defined $t;
+  
+  my $bps = $t->bits_per_sample;
+  
+  die "Could not determine TIFF bits per sample file for reading" unless defined $bps;
+  
+  die "Cannot process TIFF files with more than on ebit per sample!" unless $bps == 1;
+  
+  my $spp = $t->samples_per_pixel;
+  
+  my $w = $t->image_width;
+  my $h = $t->image_length;
+  
+  my $w_margin = $w / $MARGIN_FACTOR;
+  my $h_margin = $h / $MARGIN_FACTOR;
+  
+  my $black = 0;
+  my $white = 0;
+  
+  #
+  # We omit the top and bottom margins because there is likely to be noise there,
+  # such as black strips due to page skew.
+  #
+  # We have to read the first h_margin rows, rather than skip them, because the
+  # TIFF file's compression algorithm might not support random access.
+  #
 
-#define MARGIN_FACTOR 20
+  my $scan_line_reader = $t->scan_line_reader;
+  
+  for (my $i = $h_margin; $i < ($h - $h_margin); $i++) {    
+    #
+    # We omit the left and right margins because there is likely to be noise there,
+    # such as black strips due to page skew.
+    #
+    # The setup of last_sample and run_length simulates a leading white run long
+    # enough that any actual leading white, no matter how much, will be omitted.
+    #
 
-#include <stdarg.h>
-
-typedef void (*TIFFWarningHandler)(const char* module, const char* fmt, va_list ap);
-#if 0
-typedef void (*TIFFErrorHandler)(const char* module, const char* fmt, va_list ap);
-#endif
-
-
-#if 0
-static void tiff_density_err(const char* module, const char* fmt, va_list ap)
-{
-  croak ... /* TODO??? */
-}
-#endif
-
-double tiff_directory_density(TIFF * t, TIFFWarningHandler old_warn, TIFFErrorHandler old_err) {
-  uint16             bps;     /* Bits per sample */
-  uint16             spp;     /* Image depth (samples per pixel?) */
-  uint32             w;       /* Image width */
-  uint32             h;       /* Image height */
-  tsize_t            s;       /* Size in bytes of one scanline */
-  unsigned char *    b;       /* Scanline buffer */
-  long               i;       /* Scanline index */
-  long               j;       /* Byte index */
-  long               k;       /* Bit index */
-  long               black;
-  long               white;
-  double             density;
-  uint32             w_margin; /* The margins are used to exclude part of the border */
-  uint32             h_margin;
-
-  if (t == NULL) {
-    TIFFSetWarningHandler(old_warn);
-    TIFFSetErrorHandler(old_err);
-    croak("Could not open file for reading");
-  }
-	
-  if (TIFFGetField(t, TIFFTAG_BITSPERSAMPLE, &bps) != 1) {
-    TIFFSetWarningHandler(old_warn);
-    TIFFSetErrorHandler(old_err);
-    croak("Could not determine TIFF bits per sample file for reading");
-  }
-
-  if (bps != 1) {
-    TIFFSetWarningHandler(old_warn);
-    TIFFSetErrorHandler(old_err);
-    croak("Cannot process TIFF files with more than one bit per sample!");
-  }
-
-  if (TIFFGetField(t, TIFFTAG_IMAGEDEPTH, &spp) != 1) {
-    /* It is OK for this field to be missing. We'll assume 1. */
-    spp = 1;
-  }
-
-  if (bps != 1) {
-    TIFFSetWarningHandler(old_warn);
-    TIFFSetErrorHandler(old_err);
-    croak("Cannot process TIFF files with more than one sample per pixel!");
-  }
-
-  if (TIFFGetField(t, TIFFTAG_IMAGEWIDTH, &w) != 1) {
-    TIFFSetWarningHandler(old_warn);
-    TIFFSetErrorHandler(old_err);
-    croak("Could not determine TIFF width!");
-  }
-
-  if (TIFFGetField(t, TIFFTAG_IMAGELENGTH, &h) != 1) {
-    TIFFSetWarningHandler(old_warn);
-    TIFFSetErrorHandler(old_err);
-    croak("Could not determine TIFF height!");
-  }
- 
-/*  fprintf(stderr, "Image Width  = %ld\n", w); */
-/*  fprintf(stderr, "Image Height = %ld\n", h); */
-
-  w_margin = w / MARGIN_FACTOR;
-  h_margin = h / MARGIN_FACTOR;
-
-  /*
-  ** Prepare to read the Scanlines:
-  */
-
-  s = TIFFRasterScanlineSize(t);
-
-  b = (char *)malloc(s);
-
-/*  fprintf(stdout, "Raster Scanline Size = %ld\n", (long)s); */
-
-  if (b == NULL) {
-    TIFFSetWarningHandler(old_warn);
-    TIFFSetErrorHandler(old_err);
-    croak("Could not allocate memory for scanline reading!");
-  }
-
-  black = 0;
-  white = 0;
-     
-  /*
-  ** We omit the top and bottom margins because there is likely to be noise there,
-  ** such as black strips due to page skew.
-  **
-  ** We have to read the first h_margin rows, rather than skip them, because the
-  ** TIFF file's compression algorithm might not support random access.
-  */
-
-  for (i = 0; i < (h - h_margin); i++) {
-    long          row_black;
-    long          row_white;
-    unsigned char last_sample;
-    long          run_length;
-
-    if (TIFFReadScanline(t, b, i, 0) == -1) {
-      TIFFSetWarningHandler(old_warn);
-      TIFFSetErrorHandler(old_err);
-      croak("Could not read scanline!");
-    }
-
-    if (i < h_margin) {
-      continue;
-    }
-     
-    /*
-    ** We omit the left and right margins because there is likely to be noise there,
-    ** such as black strips due to page skew.
-    **
-    ** The setup of last_sample and run_length simulates a leading white run long
-    ** enough that any actual leading white, no matter how much, will be omitted.
-    **/
-
-    row_black   = 0;
-    row_white   = 0;
-    last_sample = 0;
-    run_length  = w_margin;
-
-    for (j = w_margin; j < (w - w_margin); j++) {
-      unsigned char byte_num = j / 8;
-      unsigned char byte     = b[byte_num];
-      unsigned char bit_num  = 7 - (j % 8);
-      unsigned char sample   = (byte >> bit_num) & 0x01;
-
-      /*
-      ** We don't count row white until we see black. This omits leading and trailing
-      ** white on the row, which helps out when we have images with large blank areas,
-      ** but decent density within the areas filled in, which is what we really care
-      ** about.
-      **
-      ** We also don't count row_white when it is greater than the margin, since that
-      ** amounts to a "large" empty space, and we really want the density of *data*,
-      ** where there *is* data.
-      */
-
-      if (sample == last_sample) {
-        run_length++;
+    my $row_black = 0;
+    my $row_white = 0;
+    my $last_sample = 0;
+    my $run_length = $w_margin;
+    
+    my $scan_line = &$scan_line_reader($i);
+    
+    for (my $j = $w_margin; $j < ($w - $w_margin); $j++) {
+      my $byte_index = $j / 8;
+      my $byte = vec($scan_line, $byte_index, 8);
+      my $bit_index = 7 - ($j % 8);
+      my $bit = ($byte >> $bit_index) & 0x01;
+      my $sample = !$bit;
+  
+      #
+      # We don't count row white until we see black. This omits leading and trailing
+      # white on the row, which helps out when we have images with large blank areas,
+      # but decent density within the areas filled in, which is what we really care
+      # about.
+      #
+      # We also don't count row_white when it is greater than the margin, since that
+      # amounts to a "large" empty space, and we really want the density of *data*,
+      # where there *is* data.
+      #
+      
+      if ($sample == $last_sample) {
+        $run_length++;
       }
       else {
-        if (run_length < w_margin) {
-          if (last_sample) {
-            row_black += run_length;
+        if ($run_length < $w_margin) {
+          if ($last_sample) {
+            $row_black += $run_length;
           }
           else {
-            row_white += run_length;
+            $row_white += $run_length;
           }
         }
 
-        last_sample = sample;
-        run_length = 1;
+        $last_sample = $sample;
+        $run_length = 1;
       }
-
-#if 0
-
-      if (sample) {
-        if (row_black && row_white < w_margin) {
-          white += row_white;
-        }
-
-        row_black++;
-
-        row_white = 0;
-      }
-      else {
-        row_white++;
-      }
-
-#endif
-
     }
-
-    if (run_length < w_margin) {
-      if (last_sample) {
-        row_black += run_length;
+    
+    if ($run_length < $w_margin) {
+      if ($last_sample) {
+        $row_black += $run_length;
       }
 
-      /* We don't add trailing white runs to the row's total */
+      # We don't add trailing white runs to the row's total
     }
   
-    white += row_white;
-    black += row_black;
+    $white += $row_white;
+    $black += $row_black;
   }
-
-  free(b);
-
-  if (black + white > 0) {
-    density = (double)black / (double)(black + white);
+  
+  my $density;
+  
+  if ($black + $white > 0) {
+    no integer;
+    $density = $black / ($black + $white);
   }
   else {
-    density = -1.0;
+    $density = -1.0;
   }
 
-  return density;
+  return $density;
 }
 
-double tiff_density(char * file_name) {
-  TIFF *             t;       /* TIFF */
-  TIFFWarningHandler old_warn;
-  TIFFErrorHandler   old_err;
+sub tiff_density {
+  my $file_name = shift;
+  
+  my $tiff = MAS::TIFF::File->new($file_name);
 
-  /*
-  ** Open the TIFF file and find out some things about it.
-  */
-
-  old_warn = TIFFSetWarningHandler((TIFFWarningHandler)0);
-  old_err  = TIFFSetErrorHandler((TIFFWarningHandler)0);
-
-  t = TIFFOpen(file_name, "r");
-
-  if (t == NULL) {
-    TIFFSetWarningHandler(old_warn);
-    TIFFSetErrorHandler(old_err);
-    croak("Could not open file for reading");
-  }
-
-  double density = tiff_directory_density(t, old_warn, old_err);
-
-  TIFFClose(t);
-
-  TIFFSetWarningHandler(old_warn);
-  TIFFSetErrorHandler(old_err);
-
-  return density;
+  my ($first_ifd, ) = $tiff->ifds;
+  
+  my $density = tiff_directory_density($first_ifd);
+  
+  $tiff->close;
+  
+  undef $tiff;
+  
+  return $density;
 }
 
-void tiff_densities(char * file_name) {
-  TIFF *             t;       /* TIFF */
-  TIFFWarningHandler old_warn;
-  TIFFErrorHandler   old_err;
+sub tiff_densities {
+  my $file_name = shift;
+  
+  my $tiff = MAS::TIFF::File->new($file_name);
 
-  /*
-  ** Open the TIFF file and find out some things about it.
-  */
-
-  old_warn = TIFFSetWarningHandler((TIFFWarningHandler)0);
-  old_err  = TIFFSetErrorHandler((TIFFWarningHandler)0);
-
-  t = TIFFOpen(file_name, "r");
-
-  if (t == NULL) {
-    TIFFSetWarningHandler(old_warn);
-    TIFFSetErrorHandler(old_err);
-    croak("Could not open file for reading");
-  }
-
-  Inline_Stack_Vars;
-  Inline_Stack_Reset;
-  do {
-  	double density = tiff_directory_density(t, old_warn, old_err);
-    Inline_Stack_Push(sv_2mortal(newSVnv(density)));
-  } while (TIFFReadDirectory(t));
-  Inline_Stack_Done;
-
-  TIFFClose(t);
-
-  TIFFSetWarningHandler(old_warn);
-  TIFFSetErrorHandler(old_err);
-
-  return;
+  my @densities = map { $_ = tiff_directory_density($_) } $tiff->ifds;
+  
+  $tiff->close;
+  
+  undef $tiff;
+  
+  return @densities;
 }
 
+1;
 
--- t/examples.t
+++ t/examples.t
@@ -4,27 +4,29 @@ use Image::Density::TIFF;
 
 my $tolerance = 0.000001;
 
+my @expected = (0.223278, 0.130488, 0.082122);
+
 my $original = tiff_density("t/original.tif");
-print "1 (original): ", $original, "\n";
-print "not " unless abs($original - 0.223277) <= $tolerance;
+printf "1 (original): %8.6f (expecting %8.6f)\n", $original, $expected[0];
+print "not " unless abs($original - $expected[0]) <= $tolerance;
 print "ok 1\n";
 
 my $diffuse = tiff_density("t/diffuse.tif");
-print "2 (diffuse): ", $diffuse, "\n";
-print "not " unless abs($diffuse - 0.130487) <= $tolerance;
+printf "2 (diffuse): %8.6f (expecting %8.6f)\n", $diffuse, $expected[1];
+print "not " unless abs($diffuse - $expected[1]) <= $tolerance;
 print "ok 2\n";
 
 my $noisy = tiff_density("t/noisy.tif");
-print "3 (noisy): ", $noisy, "\n";
-print "not " unless abs($noisy - 0.082121) <= $tolerance;
+printf "3 (noisy): %8.6f (expecting %8.6f)\n", $noisy, $expected[2];
+print "not " unless abs($noisy - $expected[2]) <= $tolerance;
 print "ok 3\n";
 
 my @multi = tiff_densities("t/multi.tif");
-print "4 (multi): ", join(", ", @multi), "\n";
+print "4 (multi): ", join(", ", map { $_ = sprintf("%8.6f", $_) } @multi), " (expecting ", join(", ", map { $_ = sprintf("%8.6f", $_) } @expected), ")\n";
 print "not " unless (scalar(@multi) == 3)
-  && (abs($multi[0] - 0.223277) <= $tolerance)
-  && (abs($multi[1] - 0.130487) <= $tolerance)
-  && (abs($multi[2] - 0.082121) <= $tolerance);
+  && (abs($multi[0] - $expected[0]) <= $tolerance)
+  && (abs($multi[1] - $expected[1]) <= $tolerance)
+  && (abs($multi[2] - $expected[2]) <= $tolerance);
 print "ok 4\n";
 
 exit 0;
